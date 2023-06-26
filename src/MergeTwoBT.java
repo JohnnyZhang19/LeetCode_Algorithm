641165f590dfd8e5c641ecf1b0abcbcd1471212f
JohnnyZhang19 <zonglin.zhang@ucalgary.ca>
2023-06-26 11:11:29 -0600

    Create MergeTwoBT.java

---
+++ src/MergeTwoBT.java
@@ -0,0 +1,17 @@
+
+public class MergeTwoBT {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null){
+            return null;
+        }else if(root1 == null){
+            return root2;
+        }else if(root2 == null){
+            return root1;
+        }else{
+            TreeNode node = new TreeNode(root1.val + root2.val);
+            node.left = mergeTrees(root1.left, root2.left);
+            node.right = mergeTrees(root1.right, root2.right);
+            return node;
+        }
+    }   
+}
