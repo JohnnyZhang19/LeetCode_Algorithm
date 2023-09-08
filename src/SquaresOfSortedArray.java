732d6ed4d45e5a10edd1c91d5f4f6a50058b1421
JohnnyZhang19 <zonglin.zhang@ucalgary.ca>
2023-09-08 10:28:00 -0600

    Create SquaresOfSortedArray.java

---
+++ src/SquaresOfSortedArray.java
@@ -0,0 +1,17 @@
+public class SquaresOfSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int[] res = new int[nums.length];
+        int i = 0, j = nums.length-1, k = nums.length-1;
+        while(i <= j) {
+            if(nums[i]*nums[i] > nums[j]*nums[j]) {
+                res[k--] = nums[i]*nums[i];
+                i++;
+            }
+            else {
+                res[k--] = nums[j]*nums[j];
+                j--;
+            }
+        }
+        return res;
+    }
+}
