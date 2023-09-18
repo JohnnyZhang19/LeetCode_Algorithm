70d0cc919640297a846bb31c24a8f08dadcae494
JohnnyZhang19 <zonglin.zhang@ucalgary.ca>
2023-09-18 11:14:39 -0600

    Create SpiralMatrix2.java
    Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
    
    Example 1:
    
    Input: n = 3
    Output: [[1,2,3],[8,9,4],[7,6,5]]
    Example 2:
    
    Input: n = 1
    Output: [[1]]

---
+++ src/SpiralMatrix2.java
@@ -0,0 +1,26 @@
+public class SpiralMatrix2 {
+    public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+        int num = 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                res[top][i] = num++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                res[i][right] = num++;
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                res[bottom][i] = num++;
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                res[i][left] = num++;
+            }
+            left++;
+        }
+        return res;
+    }
+}
