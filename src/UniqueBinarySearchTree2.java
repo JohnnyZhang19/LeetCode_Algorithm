cbc35ba17ac75cee9bdc2ff9c2b27db3835d7b0f
JohnnyZhang19 <zonglin.zhang@ucalgary.ca>
2023-09-28 10:28:52 -0600

    Create UniqueBinarySearchTree2.java
    Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
    
    Example 1:
    
    Input: n = 3
    Output: [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
    Example 2:
    
    Input: n = 1
    Output: [[1]]

---
+++ src/UniqueBinarySearchTree2.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class UniqueBinarySearchTree2 {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> list = new ArrayList<>();
+        if (start > end) {
+            list.add(null);
+            return list;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left = generateTrees(start, i - 1);
+            List<TreeNode> right = generateTrees(i + 1, end);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+}
