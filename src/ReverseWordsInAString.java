71511b23034e6dd4eda8722c34db97f455210d29
JohnnyZhang19 <zonglin.zhang@ucalgary.ca>
2023-10-04 13:30:51 -0600

    Create ReverseWordsInAString.java
    Given an input string s, reverse the order of the words.
    
    A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
    
    Return a string of the words in reverse order concatenated by a single space.
    
    Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
    
    Example 1:
    
    Input: s = "the sky is blue"
    Output: "blue is sky the"
    Example 2:
    
    Input: s = "  hello world  "
    Output: "world hello"
    Explanation: Your reversed string should not contain leading or trailing spaces.
    Example 3:
    
    Input: s = "a good   example"
    Output: "example good a"
    Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.

---
+++ src/ReverseWordsInAString.java
@@ -0,0 +1,13 @@
+public class ReverseWordsInAString {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            if (!words[i].isEmpty()) {
+                sb.append(words[i]);
+                sb.append(" ");
+            }
+        }
+        return sb.toString().trim(); // trim() removes the trailing space.
+    }
+}
