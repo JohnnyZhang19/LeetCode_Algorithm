6992239e91baa8af2a5ddc8784b1569794118b21
JohnnyZhang19 <zonglin.zhang@ucalgary.ca>
2023-10-24 17:45:58 -0600

    Create FindLargestValueInEachTreeRow.java
    Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
    
    Example 1:
    
    Input: root = [1,3,2,5,3,null,9]
    Output: [1,3,9]
    Example 2:
    
    Input: root = [1,2,3]
    Output: [1,3]

---
+++ FindLargestValueInEachTreeRow.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class FindLargestValueInEachTreeRow {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                max = Math.max(max, cur.val);
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+            res.add(max);
+        }
+        return res;
+    }
+}
